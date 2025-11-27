00f572f874845f2d573c56a7daaf5847bc4bde6a
Sebitiger <sep76th3boss3@gmail.com>
2025-11-27 15:26:50 +0100

    Update twitterClient.js

--- twitterClient.js
+++ twitterClient.js
@@ -1,43 +1,66 @@
 // twitterClient.js
+
 import { TwitterApi } from "twitter-api-v2";
 
 const client = new TwitterApi({
   appKey: process.env.API_KEY,
   appSecret: process.env.API_SECRET,
   accessToken: process.env.ACCESS_TOKEN,
-  accessSecret: process.env.ACCESS_SECRET
+  accessSecret: process.env.ACCESS_SECRET,
 });
 
-export async function postTweet(text, mediaBuffer = null) {
+/**
+ * Post a tweet without image.
+ * If replyToId is provided, posts as a reply.
+ */
+export async function postTweet(text, replyToId = null) {
   try {
-    let mediaId = null;
+    const payload = { text };
 
-    if (mediaBuffer) {
-      const uploadRes = await client.v1.uploadMedia(mediaBuffer, { type: "image" });
-      mediaId = uploadRes;
+    if (replyToId) {
+      payload.reply = { in_reply_to_tweet_id: replyToId };
     }
 
-    const res = await client.v2.tweet({
-      text,
-      ...(mediaId ? { media: { media_ids: [mediaId] } } : {})
-    });
-
-    console.log("[Twitter] Tweet posted with id:", res.data.id);
-    return res.data.id;
+    const res = await client.v2.tweet(payload);
+    const tweetId = res.data.id;
+    console.log("[Twitter] Tweet posted. ID:", tweetId);
+    return tweetId;
   } catch (err) {
-    console.error("[Twitter Error posting tweet]", err);
+    console.error("[Twitter] Error posting tweet:", err);
     throw err;
   }
 }
 
-export async function replyTweet(text, replyToId) {
+/**
+ * Post a tweet with an image (Buffer).
+ * If replyToId is provided, posts as a reply.
+ */
+export async function postTweetWithImage(text, imageBuffer, replyToId = null) {
   try {
-    const res = await client.v2.tweet({
-      text,
-      reply: { in_reply_to_tweet_id: replyToId }
-    });
-    console.log("[Twitter] Reply posted:", res.data.id);
+    let mediaId = null;
+
+    if (imageBuffer) {
+      console.log("[Twitter] Uploading media…");
+      mediaId = await client.v1.uploadMedia(imageBuffer, { type: "image" });
+      console.log("[Twitter] Media uploaded. ID:", mediaId);
+    }
+
+    const payload = { text };
+
+    if (mediaId) {
+      payload.media = { media_ids: [mediaId] };
+    }
+
+    if (replyToId) {
+      payload.reply = { in_reply_to_tweet_id: replyToId };
+    }
+
+    const res = await client.v2.tweet(payload);
+    const tweetId = res.data.id;
+    console.log("[Twitter] Tweet with image posted. ID:", tweetId);
+    return tweetId;
   } catch (err) {
-    console.error("[Twitter reply error]", err);
+    console.error("[Twitter] Error posting tweet with image:", err);
+    throw err;
   }
 }
